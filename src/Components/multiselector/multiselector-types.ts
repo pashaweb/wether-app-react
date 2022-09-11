94407152e9c3fb40bb127931a49ffcc1d4ff3a8e
pavelk <pavel.k@selectmedia.com>
2022-09-11 14:29:49 +0200

    Refuctor

--- src/Components/multiselector/multiselector-types.ts
+++ src/Components/multiselector/multiselector-types.ts
@@ -1,4 +1,5 @@
 
+
 export interface SearchResultProps {
     resultText: string;
     searchQuery: string;
@@ -14,7 +15,12 @@ export interface SearchResultListItemProps {
 
 export interface SearchBoxProps { 
     placeholder: string;
+    lableText: string;
+    searchResults: SearchResultListItemProps[];
     onSearch: CallbackFunctionVariadicAnyReturn;
+    searchQuery?: string;
+    minSearchLength?: number;
+    searchTimeout?: number;
 }
 
 type CallbackFunctionVariadicAnyReturn = (...args: any[]) => any;
